e0dec2258c31a5d387a54e6954c6f0769af1cadc
shanth kuamer <shanth923@gmail.com>
2020-01-25 19:06:19 +0530

    dfgdfgdf

--- practice/ViewController.swift
+++ practice/ViewController.swift
@@ -12,9 +12,72 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        var string = "hello world"
+        
+        var empty = ""
+        var empty1 = ""
 
+        
+//  hello orld ith one by one multiple
+        
+//        for k in string{
+//
+//            empty = empty + "\(k)"
+//
+//         print(empty)
+//        }
+        
+        for k in string{
+            
+            empty1 = empty + "\(k)"
+         
+        }
+  print(empty1)
+    
+        
+        
+        var array = ["134tgrtiu689dfg"]
+
+        var alpha = "abcdefghijklmnopqrstuvwxyz"
+        var  alphbets = CharacterSet.init(charactersIn: alpha)
+        
+        var arrays = [String]()
+        
+        var alphonly = [String]()
+        
+        array.append(alpha)
+        for x in array{
+            
+            arrays.append(alpha)
+            for i in arrays{
+                
+                if(x == i){
+                  
+                    alphonly.append(x)
+                    
+                    
+                }
+                
+            }
+            
+            
+        }
+     
+        print(alphonly)
+}
+ 
+    
+    
+    func data(){
+        
+        
+        
+        
+        
+    }
+    
+    
+    
 
 }
-
---
+++ practice/ViewController67.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController67.swift
+//  practice
+//
+//  Created by R Shantha Kumar on 1/25/20.
+//  Copyright © 2020 R Shantha Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController67: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    func final(){
+        
+        
+        
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
